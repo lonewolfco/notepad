7a311bf865736d6a17686f8461e01005c63244d7
Lo Selby <laurenxselby@gmail.com>
2022-01-29 18:05:39 -0800

    gitignore edits server.js route fix

---
+++ routes/notes.js
@@ -0,0 +1,5 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+
+
+
--- server.js
+++ server.js
@@ -1,37 +1,63 @@
+// dependencies
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
-const PORT = process.env.port || 3001;
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Import custom middleware, "cLog"
-app.use(clog);
 
-// Middleware for parsing JSON and urlencoded form data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// const api = require('./routes/index');
+// const noteJson = require('./db/db');
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// Parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// require notes JSON file
+const noteLog = require('./db/db.json');
+
+// GET Route for homepage
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, '/public/index.html') );
+
+});
+
+// GET Request: /notes route
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html') );
+
+});
+
+// GET Request: read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+  res.json(noteLog);
+});
+
+// POST Request: 
+app.post('/api/notes', (req, res) => {
+    // create data
+
+    // access the new note data from 'req'
+
+    // push it to exisiting list of notes
+
+    // write my updated note list to the 'db.json' file
+    res.json({ success: true});
+});
+
 
 // Wildcard route to direct users back to homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html') );
+});
 
+
+
+// listening for when the server is connected to the PORT
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
